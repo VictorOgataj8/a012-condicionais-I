4427137378b9e8d0ca663781b4301dba9ab68a7f
VictorOgataj8 <victor.ogataj8@gmail.com>
2022-09-08 15:10:37 -0300

    exercicio 3 feito

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,12 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade= prompt('Escreva a sua nacionalidade')
+if(nacionalidade==="brasileira"){
+    console.log('Esta pessoa e brasileira')
+    }else if(nacionalidade==="argentina"){
+    console.log('Esta pessoa e argentina')
+    }else if(nacionalidade==='uruguaia'){
+    console.log("chilena")
+    }else if(nacionalidade==="colombiana"){
+        console.log('Esta pessoa e colombiana')
+    }else{
+        console.log("nacionalidade não encontrada")
+    }
