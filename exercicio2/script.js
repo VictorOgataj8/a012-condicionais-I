a14711c2968d6f8c18ecaf5e479a6e9c8564a1a9
VictorOgataj8 <victor.ogataj8@gmail.com>
2022-09-08 15:04:50 -0300

    exercico 2 feito

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,11 +1,21 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade= Number(prompt('Insira sua idade'))
+let IsMaiorDeIdade
+let TerminouEM= confirm('Terminou o ensino medio?')
+let isCursandoOutraFaculdade= prompt('esta cursando a faculdade?')
 
 if(idade>=18){
-    alert('Maior de idade')
+    console.log('Maior de idade')
 
 }else{
-    alert('Menor de idade')
+    console.log('Menor de idade')
 }
-if(isMaiorIdade)
+if(TerminouEM===true){
+    console.log('A pessoa terminou o ensimo medio')
+}else{
+    console.log('A pessoa nao terminou o ensino medio')
+}
+if(isCursandoOutraFaculdade === "sim"){
+    console.log('Esta cursando uma faculdade')
+}else{
+    console.log('Nao esta cursando faculdade')
+}
